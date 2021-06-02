0eaabd71488d014f3dd9d7c97663e1b543b973dc
Michael Tobis <tobi4120@mylaurier.ca>
2021-06-01 21:21:30 -0400

    slider added

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,19 @@
-import { useEffect, useState } from "react";
-//import { get_photo } from "../helper_functions/get_pictures";
+import { useEffect, useState, useRef } from "react";
 import Image from  "../components/image";
 
 export default function Home() {
     const [photo, set_photoURL] = useState()
+    const [split, _set_split] = useState(0.5)
     const max = 700
     const min = 200 
+
+    // Split ref
+    const splitRef = useRef(split);
+
+    const set_split = data => {
+        splitRef.current = data; // keep updated
+        _set_split(data);
+    };
 
     useEffect(() => {
         // Get a random photo and store the URL in a useState variable
@@ -13,20 +21,27 @@ export default function Home() {
 
         // Add event listener to listen when the user clicks a key on the keyboard
         window.addEventListener('keydown', handleKeyDown);
+
+        // Remove listener
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+
     }, []);
 
     // Function to get a random photo
     const get_photo = () => {
-        // Get a random num 
-        const rand_num = Math.round(Math.random())
 
-        if (rand_num === 0)
+        // Get a random num 
+        const rand_num = Math.random()
+
+        if (rand_num > splitRef.current)
             return `https://placekitten.com/${Math.round(Math.random() * (max - min) + min)}/${Math.round(Math.random() * (max - min) + min)}`
         else
-            return `https://source.unsplash.com/random/${Math.round(Math.random() * (max - min) + min)}x${Math.round(Math.random() * (max - min) + min)}`
+            return `https://source.unsplash.com/random/${Math.round(Math.random() * (max - min) + min)}x${Math.round(Math.random() * (max - min) + min)}`;
     }
 
-    // Refresh the photo if the user clicks on the 'up' arrow key
+    // Event listener to refresh the photo if the user clicks on the 'up' arrow key
     const handleKeyDown = (event) => {
         if (event.keyCode === 38) {
 
@@ -35,11 +50,20 @@ export default function Home() {
         }
     };
 
+    // Handle slider change
+    const changeSlider = (e) => {
+        set_split((e.target.value)/100)
+    }
+
     return (
         <div>
-            Home
             <Image image={photo} />
-            <p>{photo}</p>
+            
+            <div className="slider">
+                <p>Place Kitten: {Math.round(100-split*100)}</p>
+                <input type="range" onChange={changeSlider} value={split*100} min="0" max="100" />
+                <p>Unsplash: {Math.round(split*100)}</p>
+            </div>
         </div>
     )
 }
